31cb52282cfaf6d26c01370b9bd37bedb711480b
Josh Levy <joshaaronlevy@gmail.com>
2019-02-10 22:40:35 -0700

    Added Heroku

--- routes/skills.js
+++ routes/skills.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const queries = require('../queries');
+const queries = require('../queries/skills');
 
 router.get('/', (request, response, next) => {
   queries.list('skills').then(skills => {
