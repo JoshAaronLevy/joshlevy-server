1d20e9fd314a56366c4c2e92bc9c86dc88ca74c7
Josh Levy <joshaaronlevy@gmail.com>
2019-02-10 22:28:18 -0700

    Added code for app index db routes and queries

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+const skills = require('./routes/skills');
+const projects = require('./routes/projects');
+const jobs = require('./routes/jobs');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/skills', skills);
+app.use('/projects', projects);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err.stack : {}
+  });
+});
+
+module.exports = app;
