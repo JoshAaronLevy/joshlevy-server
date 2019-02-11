5c52f98949771ee1695cf74862b40435c6ddb6d1
Josh Levy <joshaaronlevy@gmail.com>
2019-02-10 22:59:36 -0700

    Added projects to seed file

--- seeds/02_projects.js
+++ seeds/02_projects.js
@@ -1,7 +1,8 @@
 exports.seed = function (knex, Promise) {
   return knex('projects').del()
     .then(function () {
-      return knex('projects').insert([{
+      return knex('projects').insert([
+      {
         id: 1,
         name: 'Bad Movie Night',
         date: 'Jul 2018',
@@ -21,6 +22,47 @@ exports.seed = function (knex, Promise) {
         youtubeURL: null,
         deployedURL: 'https://joshlevy.tech/',
         githubURL: 'https://github.com/JoshAaronLevy/portfolio'
-      }]);
+      }, {
+        id: 3,
+        name: 'Dev Videos',
+        date: 'Aug 2018',
+        snippet: `Full CRUD. Find developer videos easily with intuitive filters. Built with Vue.js and MongoDB.`,
+        description: `Full CRUD. Find developer videos easily with intuitive filters. Built with Vue.js and MongoDB.`,
+        gifURL: 'https://s3-us-west-2.amazonaws.com/joshlevyportfolio/projects/React-Logo.png',
+        youtubeURL: null,
+        deployedURL: 'https://g90-videos.firebaseapp.com/',
+        githubURL: 'https://github.com/JoshAaronLevy/gvideos-client'
+      }, {
+        id: 4,
+        name: 'PostureMom',
+        date: 'Sep 2018',
+        snippet: `Capstone project. Desktop app created with Vue.js, Electron, Google TensorFlow.js Machine Learning, and Google Cloud Functions.`,
+        description: `Capstone project. Desktop app created with Vue.js, Electron, Google TensorFlow.js Machine Learning, and Google Cloud Functions.`,
+        gifURL: 'https://s3-us-west-2.amazonaws.com/joshlevyportfolio/projects/React-Logo.png',
+        youtubeURL: `https://www.youtube.com/watch?v=M1_SJuy5KPI&feature=youtu.be`,
+        deployedURL: null,
+        githubURL: 'https://github.com/JoshAaronLevy/posturemom-app/'
+      }, {
+        id: 5,
+        name: 'Coding Roadmap',
+        date: 'Oct 2018',
+        snippet: `A blog about all things web development.`,
+        description: `A blog about all things web development.`,
+        gifURL: 'https://s3-us-west-2.amazonaws.com/joshlevyportfolio/projects/React-Logo.png',
+        youtubeURL: null,
+        deployedURL: 'https://codingroadmap.com/',
+        githubURL: 'https://github.com/JoshAaronLevy/coding-roadmap'
+      }, {
+        id: 6,
+        name: 'Angular Books',
+        date: 'Nov 2018',
+        snippet: `A side project showcasing my 10 favorite books. Built with Angular 6.`,
+        description: `A side project showcasing my 10 favorite books. Built with Angular 6.`,
+        gifURL: 'https://s3-us-west-2.amazonaws.com/joshlevyportfolio/projects/React-Logo.png',
+        youtubeURL: null,
+        deployedURL: 'https://angular-bookstore.firebaseapp.com/books',
+        githubURL: 'https://github.com/JoshAaronLevy/angular-bookstore'
+      }
+    ]);
     });
 };
